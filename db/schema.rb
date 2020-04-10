5be06f4342a36409d3c392311af3cc02afca4f62
ottocedeno <otto@creativespaceclub.com>
2020-04-10 16:31:53 -0400

    Deployed Category Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200410201538) do
+ActiveRecord::Schema.define(version: 20200410202733) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "exercises", force: :cascade do |t|
     t.string   "name"
