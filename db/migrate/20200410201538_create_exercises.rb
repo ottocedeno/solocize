639cfbfae0a1c10c2be7233c98790988222563d8
ottocedeno <otto@creativespaceclub.com>
2020-04-10 16:26:43 -0400

    Deployed Exercise Model

--- db/migrate/20200410201538_create_exercises.rb
+++ db/migrate/20200410201538_create_exercises.rb
@@ -1,7 +1,8 @@
 class CreateExercises < ActiveRecord::Migration
   def change
     create_table :exercises do |t|
-
+      t.string :name
+      t.integer :rep_count
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200410200223) do
+ActiveRecord::Schema.define(version: 20200410201538) do
+
+  create_table "exercises", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "rep_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
