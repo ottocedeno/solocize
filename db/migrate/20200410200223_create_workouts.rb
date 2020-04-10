da16214b1c09d24fa97c80c1dace8a60221c6e71
ottocedeno <otto@creativespaceclub.com>
2020-04-10 16:13:28 -0400

    Deployed Workout Model + Association to User

--- db/migrate/20200410200223_create_workouts.rb
+++ db/migrate/20200410200223_create_workouts.rb
@@ -1,7 +1,10 @@
 class CreateWorkouts < ActiveRecord::Migration
   def change
     create_table :workouts do |t|
-
+      t.string :name
+      t.string :description
+      t.integer :minutes
+      t.integer :user_id
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200410184223) do
+ActiveRecord::Schema.define(version: 20200410200223) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 20200410184223) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "workouts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "minutes"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
