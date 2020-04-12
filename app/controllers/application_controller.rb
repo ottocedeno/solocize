c1d66638881d1d7421c84271d25552dc80724a3a
ottocedeno <otto@creativespaceclub.com>
2020-04-12 15:40:38 -0400

    Added session functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
+    set :session_secret, "84215ANDA7W00DC1RCL3"
     set :public_folder, 'public'
     set :views, 'app/views'
   end
