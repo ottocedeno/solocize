0eb01fb37428a11d0c00fac4724003bbb3dc7b56
ottocedeno <otto@creativespaceclub.com>
2020-04-12 23:36:55 -0400

    Workout .featured class method added

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,4 +2,8 @@ class Workout < ActiveRecord::Base
   belongs_to :user
   has_many :workout_exercises
   has_many :exercises, through: :workout_exercises
+
+  def self.featured
+    self.all.sample
+  end
 end
