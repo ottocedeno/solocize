b2999ed8d7d369d8d412c6bf59d7a8fa85678d44
ottocedeno <otto@creativespaceclub.com>
2020-04-15 19:34:27 -0400

    Cleaned up User Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,12 @@ class UsersController < ApplicationController
 
   ####### GET /users/ #######
   get "/users" do
-    @users = User.all
-    erb :"/users/index"
+    if logged_in?
+      @users = User.all
+      erb :"/users/index"
+    else
+      redirect '/login'
+    end
   end
 
   ####### GET /users/:slug #######
@@ -14,21 +18,5 @@ class UsersController < ApplicationController
     else
       redirect '/login'
     end
-
-  end
-
-  # GET: /users/5/edit
-  get "/users/:id/edit" do
-    erb :"/users/edit.html"
-  end
-
-  # PATCH: /users/5
-  patch "/users/:id" do
-    redirect "/users/:id"
-  end
-
-  # DELETE: /users/5/delete
-  delete "/users/:id/delete" do
-    redirect "/users"
   end
 end
